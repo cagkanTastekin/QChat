e353e2f96ced804534f3ec9783aa75b17bec4398
Cagkan Tastekin <tastekincagkan@gmail.com>
2019-10-30 09:41:09 +0100

    Pods installed

---
+++ QChat/Controller/SignInViewCont.swift
@@ -0,0 +1,45 @@
+//
+//  SignInViewCont.swift
+//  QChat
+//
+//  Created by ÇağkanTaştekin on 2019. 10. 30..
+//  Copyright © 2019. ÇağkanTaştekin. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewCont: UIViewController {
+
+    // MARK: Outlets
+    @IBOutlet weak var imgBackground: UIImageView!
+    @IBOutlet weak var btnBackground: UIButton!
+    @IBOutlet weak var imgLogo: UIImageView!
+    @IBOutlet weak var lblTag: UILabel!
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var btnSignIn: UIButton!
+    @IBOutlet weak var btnSignUp: UIButton!
+    
+    // MARK: ViewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: IBActions
+    @IBAction func signIn(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func signUp(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func backgroundHelper(_ sender: UIButton) {
+        
+    }
+    
+    // MARK: HelperFunctions
+    
+    // MARK: GoToApp
+
+}
---
+++ QChat/Controller/SignUpViewCont.swift
@@ -0,0 +1,44 @@
+//
+//  SignUpViewCont.swift
+//  QChat
+//
+//  Created by ÇağkanTaştekin on 2019. 10. 30..
+//  Copyright © 2019. ÇağkanTaştekin. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewCont: UIViewController {
+    
+    // MARK: Outlets
+    @IBOutlet weak var imgBackground: UIImageView!
+    @IBOutlet weak var btnBackground: UIButton!
+    @IBOutlet weak var imgLogo: UIImageView!
+    @IBOutlet weak var lblTag: UILabel!
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var txtRepeatPassword: UITextField!
+    @IBOutlet weak var btnSignUp: UIButton!
+    @IBOutlet weak var btnCancel: UIButton!
+    
+    // MARK: ViewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+   
+    // MARK: IBActions
+    @IBAction func signUp(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func cancel(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func backgroundHelper(_ sender: UIButton) {
+        
+    }
+    
+    //MARK: HelperFunctions
+    
+}
